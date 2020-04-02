18bee317ccc52b585f8e986601b4f4b6250bd524
RikaToyohara <rikachan814sun@gmail.com>
2020-04-02 02:15:27 +0000

    ユーザー登録ボタンの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end  
+    
   def new
+    @user = User.new #form_withの引数で必要となるユーザーオブジェクトを作成、インスタンス変数に代入
   end
 end
