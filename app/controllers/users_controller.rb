18575e97276aab936513cd37f897bd63690ed1a9
RikaToyohara <rikachan814sun@gmail.com>
2020-04-03 12:16:57 +0000

    フラッシュメッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new #form_withの引数で必要となるユーザーオブジェクトを作成、インスタンス変数に代入
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      ridirect_to @user
+    else
+      render :new
+    end
+  end 
+  
+  private 
+        # このキー以降のコードをわかりやすくするため、以降字下げ
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
+
