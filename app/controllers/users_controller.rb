0c5414a70a12f6709c3df393f11eeebd9af9f657
RikaToyohara <rikachan814sun@gmail.com>
2020-04-03 12:21:02 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = '新規作成に成功しました。'
-      ridirect_to @user
+      redirect_to @user
     else
       render :new
     end
