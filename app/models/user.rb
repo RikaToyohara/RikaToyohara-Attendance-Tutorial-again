093030b91b7a1cc19bf1eedb95c8302a7afe5b01
RikaToyohara <rikachan814sun@gmail.com>
2020-04-05 23:55:54 +0000

    ランダムな文字列を生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,27 @@ class User < ApplicationRecord
   
   
   validates :name, presence: true, length: {maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # Rubyでは定数を定義する時は大文字のスネークケースで記述することが一般的
   validates :email, presence: true, length: {maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true # 一意性　他に同じデータがないという意味end
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 } # 存在性presence
+ 
+ # 渡された文字列のハッシュ値を返す 
+  def User.digest(string)
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end  
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end  
 end
