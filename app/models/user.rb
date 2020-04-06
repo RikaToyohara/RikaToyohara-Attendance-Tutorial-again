c7a4e7dd060660ce0152fee78a9b9e288c7b2db2
RikaToyohara <rikachan814sun@gmail.com>
2020-04-06 00:25:33 +0000

    永続的セッションの記憶

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # remember_tokenという仮想の属性を作成
+  attr_accessor :remember_token
   before_save { self.email = email.downcase } # 小文字に変換
   
   
@@ -25,5 +27,15 @@ class User < ApplicationRecord
   # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
+  end 
+  
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end  
+  
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end  
 end
