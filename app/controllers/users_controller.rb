ac566b87bda376a93bd7351a8bf4e3832c827473
RikaToyohara <rikachan814sun@gmail.com>
2020-04-06 07:15:41 +0000

    ユーザー情報更新ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
     end
   end 
   
+  def edit
+    @user = User.find(params[:id])
+  end  
+  
   private 
         # このキー以降のコードをわかりやすくするため、以降字下げ
     def user_params
